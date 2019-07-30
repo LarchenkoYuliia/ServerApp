cfd45a3214d3e57824437209c38eb9b5a5b285ba
Yuliia Larchenko <yuliia.larchenko@gmail.com>
2019-07-30 22:13:00 +0300

    Добавлено разбиение на папки по репозиториям. Доработан репозиторий пользователя. Добавлен репозиторий справочника наименований комнат. Дописаны todo для последующей реализации

---
+++ ServerApp/RoomNames/RoomNames.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerApp
+{
+    public class RoomNames
+    {
+        /// <summary>
+        /// Уникальный идентификатор
+        /// </summary>
+        public int RoomNameId { get; set; }
+
+        /// <summary>
+        /// Название комнаты
+        /// </summary>
+        public string RoomName { get; set; }
+    }
+}
---
+++ ServerApp/RoomNames/RoomNamesRepository.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+
+namespace ServerApp
+{
+    public class RoomNamesRepository : IRoomNamesRepository
+    {
+        public List<RoomNames> GetRooms(string login)
+        {
+            //todo: базу переделать так, чтобы добавить таблицу-справочник с названиями комнат 
+            //и возвращать именно список названий комнат
+
+            using (var connection = new SqlConnection(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString))
+            {
+                var sqlRequest =
+                    string.Format(@"select roomNmame from rooms join users on users.userid = rooms.userid join roomnames on rooms.roomnamesid = roomnames.roomnamesid WHERE login = '{0}'", login);
+                using (var cmd = new SqlCommand(sqlRequest, connection))
+                {
+                    connection.Open();
+                    var sqlDataReader = cmd.ExecuteReader();
+                    var rooms = new List<RoomNames>();
+                    while (sqlDataReader.Read())
+                    {
+                        //var room = new Room
+                        //{
+
+                        //}
+                    }
+
+                    return rooms;
+                }
+            }
+        }
+    }
+}
