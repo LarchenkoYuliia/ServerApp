81da12fe4f1f29b1ef04fc557c880d50039b8dce
Yuliia Larchenko <yuliia.larchenko@gmail.com>
2019-08-17 18:46:48 +0300

    Добавлена библиотека лампы

--- ServerApp/Program.cs
+++ ServerApp/Program.cs
@@ -10,6 +10,10 @@ namespace ServerApp
     {
         static void Main(string[] args)
         {
+            for(int i = 0; i < 15; i++)
+            {
+                Console.WriteLine("123");
+            }
         }
     }
 }
