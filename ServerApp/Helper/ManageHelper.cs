38767ade2bb50ff78af9a3017a0516ffb41526f6
Yuliia Larchenko <yuliia.larchenko@gmail.com>
2019-08-17 20:23:32 +0300

    Добавлен хелпер для проверки вывода данных. Промежуточный коммит.

---
+++ ServerApp/Helper/ManageHelper.cs
@@ -0,0 +1,49 @@
+﻿using System.Data;
+
+namespace ServerApp.Helper
+{
+    public class ManageHelper
+    {
+        private IUserRepository _userRepository;
+
+        ManageHelper()
+        {
+            _userRepository = new UserRepository();
+        }
+
+        public User Retrieve(int userId)
+        {
+            return _userRepository.Retrieve(userId);
+        }
+
+        public User Retrieve(string login)
+        {
+            return _userRepository.Retrieve(login);
+        }
+
+        public string RetrieveName(string login)
+        {
+             return _userRepository.RetrieveName(login);
+        }
+
+        public bool CheckUser(string login, string password)
+        {
+            return _userRepository.CheckUser(login, password);
+        }
+
+        public DataTable Retrieve()
+        {
+            return _userRepository.Retrieve();
+        }
+
+        public bool Create(User user)
+        {
+            return _userRepository.Create(user);
+        }
+
+        public void Delete(string login)
+        {
+            _userRepository.Delete(login);
+        }
+    }
+}
